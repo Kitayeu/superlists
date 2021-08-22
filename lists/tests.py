4514fdad5449dee7abf999f2ac57648f82c18d1d
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-08-22 16:21:06 +0300

    Added 'post' request
    Added functional for implementing "post" request

--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,9 @@ class HomePageTest(TestCase):
         """test: using a home template"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """test: you can save a post request"""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
