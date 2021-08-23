b8632f5840a13482cafa8576fd1711bc8aacda5f
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-08-23 14:15:13 +0300

    New URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -21,22 +21,12 @@ class HomePageTest(TestCase):
         """test: redirects after a post request"""
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/one-and-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         """test: saves items only when needed"""
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_displays_all_list_items(self):
-        """test: all the list items are displayed"""
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -59,3 +49,22 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+    """list view test"""
+
+    def test_uses_list_template(self):
+        """test: the list template is used"""
+        response = self.client.get('/lists/one-and-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
+        """test: all the list items are displayed"""
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/one-and-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,11 @@ def home_page(request):
     """home page"""
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/one-and-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+
+def view_list(request):
+    """new list"""
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
