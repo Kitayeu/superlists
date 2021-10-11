02b3c97288ef136a675222e1b630944b72c8fe01
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-11 19:35:19 +0300

    Added form to log in to 'base.html'
    Added 'LOGGING' to settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -127,3 +127,21 @@ STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
