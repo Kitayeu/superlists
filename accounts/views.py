ef4dc87bf87a75b82424550ef20ee02685a87189
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-12 11:38:01 +0300

    Added log in to 'accounts' app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages, auth
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -6,8 +7,20 @@ def send_login_email(request):
     email = request.POST['email']
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        'Use this link to log in',
         'noreply@superlists',
         [email]
     )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
+
+
+def login(request):
+    uid = request.GET.get('token')
+    user = auth.authenticate(uid=uid)
+    if user:
+        auth.login(request, user)
     return redirect('/')
