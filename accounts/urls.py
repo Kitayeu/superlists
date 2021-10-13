6b6603fd409826a592c111ae85a949f788ea5cbc
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-13 20:17:47 +0300

    Added logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.conf.urls import url
 
 from accounts import views
@@ -5,4 +6,5 @@ from accounts import views
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$', views.login, name='login'),
+    url(r'^logout$', LogoutView.as_view(next_page='/'), name='logout'),
 ]
