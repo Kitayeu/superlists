351cb09c6a4b63f0fef6fb752b57304624cf9ae0
Ivan Kitayeu <vanya.kitaev@gmail.com>
2021-10-14 17:15:49 +0300

    Changed tests to work locally and on server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 import time
 from selenium.common.exceptions import WebDriverException
 
+from .server_tools import reset_database
+
 MAX_WAIT = 10
 
 
@@ -24,9 +26,10 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self) -> None:
         self.browser.quit()
